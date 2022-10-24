be14dc8237bd4d9b8631671d6953cd4f696b773d
Alex <alex.ardiot@gmail.com>
2022-10-24 16:55:12 +0200

    test.py - fin de journée

--- test.py
+++ test.py
@@ -42,25 +42,7 @@ commentaire_utilisateur= {
     "Commentaire 4" : "Ce n'est qu'un au revoir Kévin"
 }
 
-def age(date_de_naissance:tuple) -> int:
-    """Retourne l'âge de l'utilisateur
-    :param date_de_naissance: Année, Mois, Jour
-    """
 
-    today = date.today()
-    age = today.year - date_de_naissance[0] - ((today.month, today.day) < (date_de_naissance[1], date_de_naissance[2]))
-    return age
-
-"""date_de_naissance = (1991, 4, 3)
-
-print(age(date_de_naissance))"""
-
-"""def age(birthdate):
-    today = date.today()
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    return age
-
-print(age(date(2000, 1, 1)))"""
 
 """nb_documents = forum.count_documents({})
 print(nb_documents)
@@ -71,41 +53,6 @@ id_article = blog.find_one({"Titre" : "Mon 1er article"})
 print(id_article["ID"])
 crud.update_article(id_article["ID"], "Mon 1er article", "On fais mieux")"""
 
+#crud.add_comment_article("4f6591887e147295b566ad97c812b2da9769174ebc4b5aa8e1a57a3ab459795f","Dulompont", "Thomas", "C'est beau")
 
-def ajouter_commentaire_article(ID:str, nom:str, prenom:str, commentaire:str) -> None:
-
-    nom = "dulompont"
-    prenom = "Thomas"
-    auteur = prenom + "." + nom.upper()[0]
-    heure = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    a_modifier = blog.find_one({"ID" : ID})
-    id_commentaire = hashlib.sha256((str(heure).encode()) + (auteur.encode())).hexdigest()
-
-    dicommentaire = {
-        "ID Commentaire" : id_commentaire,
-        "Auteur" : auteur,
-        "Date" : heure,
-        "Commentaire" : commentaire
-    }
-
-    a_modifier["Commentaires"].append(dicommentaire)
-
-    blog.update_one({"ID" : ID}, {"$set" : {"Commentaires" : a_modifier["Commentaires"]}})
-
-
-#ajouter_commentaire_article("4f6591887e147295b566ad97c812b2da9769174ebc4b5aa8e1a57a3ab459795f", "Dulompont", "Thomas", "C'est moins")
-
-"""a_modifier = blog.find_one({"ID" : "4f6591887e147295b566ad97c812b2da9769174ebc4b5aa8e1a57a3ab459795f"})
-print(len(a_modifier["Commentaires"]))
-nb = 0
-
-for key in a_modifier:
-    nb += 1
-
-print(nb)"""
-
-heure = "2022-10-21 16:42:31"
-auteur = "Thomas.D"
-id_commentaire = hashlib.sha256((str(heure).encode()) + (auteur.encode())).hexdigest()
-print(id_commentaire)
+crud.delete_comment_article("4f6591887e147295b566ad97c812b2da9769174ebc4b5aa8e1a57a3ab459795f", "b7f740fa1ca6f862a1625e1ecc04ce385c941fd9594988f1576f36932fcd60fa")
